bfd01580300850c4084f9a794c4d9b6fb2595d96
sigmareaver <sigmareaver@users.noreply.github.com>
2023-07-04 09:05:29 -0600

    Added --data-col and refactored code to process steps one at a time

--- main.py
+++ main.py
@@ -18,190 +18,116 @@ from fastpunct import FastPunct
 import glob
 import shutil
 
-ascii_map = [
-    [0x00AB, '"'],
-    [0x00AD, '-'],
-    [0x00B4, '\''],
-    [0x00BB, '"'],
-    [0x00F7, '/'],
-    [0x01C0, '|'],
-    [0x01C3, '!'],
-    [0x02B9, '\''],
-    [0x02BA, '"'],
-    [0x02BC, '\''],
-    [0x02C4, '^'],
-    [0x02C6, '^'],
-    [0x02C8, '\''],
-    [0x02CB, '`'],
-    [0x02CD, '_'],
-    [0x02DC, '~'],
-    [0x0300, '`'],
-    [0x0301, '\''],
-    [0x0302, '^'],
-    [0x0303, '~'],
-    [0x030B, '"'],
-    [0x030E, '"'],
-    [0x0331, '_'],
-    [0x0332, '_'],
-    [0x0338, '/'],
-    [0x0589, ':'],
-    [0x05C0, '|'],
-    [0x05C3, ':'],
-    [0x066A, '%'],
-    [0x066D, '*'],
-    [0x200B, ' '],
-    [0x2010, '-'],
-    [0x2011, '-'],
-    [0x2012, '-'],
-    [0x2013, '-'],
-    [0x2014, '-'],
-    [0x2015, '-'],
-    [0x2016, '|'],
-    [0x2017, '_'],
-    [0x2018, '\''],
-    [0x2019, '\''],
-    [0x201A, ','],
-    [0x201B, '\''],
-    [0x201C, '"'],
-    [0x201D, '"'],
-    [0x201E, '"'],
-    [0x201F, '"'],
-    [0x2032, '\''],
-    [0x2033, '"'],
-    [0x2034, '\''],
-    [0x2035, '`'],
-    [0x2036, '"'],
-    [0x2037, '\''],
-    [0x2038, '^'],
-    [0x2039, '<'],
-    [0x203A, '>'],
-    [0x203D, '?'],
-    [0x2044, '/'],
-    [0x204E, '*'],
-    [0x2052, '%'],
-    [0x2053, '~'],
-    [0x2060, ' '],
-    [0x20E5, '\\'],
-    [0x2212, '-'],
-    [0x2215, '/'],
-    [0x2216, '\\'],
-    [0x2217, '*'],
-    [0x2223, '|'],
-    [0x2236, ':'],
-    [0x223C, '~'],
-    [0x2264, '<'],
-    [0x2265, '>'],
-    [0x2266, '<'],
-    [0x2267, '>'],
-    [0x2303, '^'],
-    [0x2329, '<'],
-    [0x232A, '>'],
-    [0x266F, '#'],
-    [0x2731, '*'],
-    [0x00AB, '"'],
-    [0x00AD, '-'],
-    [0x00B4, '\''],
-    [0x00BB, '"'],
-    [0x00F7, '/'],
-    [0x01C0, '|'],
-    [0x01C3, '!'],
-    [0x02B9, '\''],
-    [0x02BA, '"'],
-    [0x02BC, '\''],
-    [0x02C4, '^'],
-    [0x02C6, '^'],
-    [0x02C8, '\''],
-    [0x02CB, '`'],
-    [0x02CD, '_'],
-    [0x02DC, '~'],
-    [0x0300, '`'],
-    [0x0301, '\''],
-    [0x0302, '^'],
-    [0x0303, '~'],
-    [0x030B, '"'],
-    [0x030E, '"'],
-    [0x0331, '_'],
-    [0x0332, '_'],
-    [0x0338, '/'],
-    [0x0589, ':'],
-    [0x05C0, '|'],
-    [0x05C3, ':'],
-    [0x066A, '%'],
-    [0x066D, '*'],
-    [0x200B, ' '],
-    [0x2010, '-'],
-    [0x2011, '-'],
-    [0x2012, '-'],
-    [0x2013, '-'],
-    [0x2014, '-'],
-    [0x2015, '-'],
-    [0x2016, '|'],
-    [0x2017, '_'],
-    [0x2018, '\''],
-    [0x2019, '\''],
-    [0x201A, ','],
-    [0x201B, '\''],
-    [0x201C, '"'],
-    [0x201D, '"'],
-    [0x201E, '"'],
-    [0x201F, '"'],
-    [0x2032, '\''],
-    [0x2033, '"'],
-    [0x2034, '\''],
-    [0x2035, '`'],
-    [0x2036, '"'],
-    [0x2037, '\''],
-    [0x2038, '^'],
-    [0x2039, '<'],
-    [0x203A, '>'],
-    [0x203D, '?'],
-    [0x2044, '/'],
-    [0x204E, '*'],
-    [0x2052, '%'],
-    [0x2053, '~'],
-    [0x2060, ' '],
-    [0x20E5, '\\'],
-    [0x2212, '-'],
-    [0x2215, '/'],
-    [0x2216, '\\'],
-    [0x2217, '*'],
-    [0x2223, '|'],
-    [0x2236, ':'],
-    [0x223C, '~'],
-    [0x2264, '<'],
-    [0x2265, '>'],
-    [0x2266, '<'],
-    [0x2267, '>'],
-    [0x2303, '^'],
-    [0x2329, '<'],
-    [0x2758, '|'],
-    [0x2762, '!'],
-    [0x27E6, '['],
-    [0x27E8, '<'],
-    [0x27E9, '>'],
-    [0x2983, '{'],
-    [0x2984, '}'],
-    [0x3003, '"'],
-    [0x3008, '<'],
-    [0x3009, '>'],
-    [0x301B, ']'],
-    [0x301C, '~'],
-    [0x301D, '"'],
-    [0x301E, '"'],
-    [0xFEFF, ' ']
-]
+ascii_map = {
+    0x232A: '>',
+    0x266F: '#',
+    0x2731: '*',
+    0x00AB: '"',
+    0x00AD: '-',
+    0x00B4: '\'',
+    0x00BB: '"',
+    0x00F7: '/',
+    0x01C0: '|',
+    0x01C3: '!',
+    0x02B9: '\'',
+    0x02BA: '"',
+    0x02BC: '\'',
+    0x02C4: '^',
+    0x02C6: '^',
+    0x02C8: '\'',
+    0x02CB: '`',
+    0x02CD: '_',
+    0x02DC: '~',
+    0x0300: '`',
+    0x0301: '\'',
+    0x0302: '^',
+    0x0303: '~',
+    0x030B: '"',
+    0x030E: '"',
+    0x0331: '_',
+    0x0332: '_',
+    0x0338: '/',
+    0x0589: ':',
+    0x05C0: '|',
+    0x05C3: ':',
+    0x066A: '%',
+    0x066D: '*',
+    0x200B: ' ',
+    0x2010: '-',
+    0x2011: '-',
+    0x2012: '-',
+    0x2013: '-',
+    0x2014: '-',
+    0x2015: '-',
+    0x2016: '|',
+    0x2017: '_',
+    0x2018: '\'',
+    0x2019: '\'',
+    0x201A: ',',
+    0x201B: '\'',
+    0x201C: '"',
+    0x201D: '"',
+    0x201E: '"',
+    0x201F: '"',
+    0x2032: '\'',
+    0x2033: '"',
+    0x2034: '\'',
+    0x2035: '`',
+    0x2036: '"',
+    0x2037: '\'',
+    0x2038: '^',
+    0x2039: '<',
+    0x203A: '>',
+    0x203D: '?',
+    0x2044: '/',
+    0x204E: '*',
+    0x2052: '%',
+    0x2053: '~',
+    0x2060: ' ',
+    0x20E5: '\\',
+    0x2212: '-',
+    0x2215: '/',
+    0x2216: '\\',
+    0x2217: '*',
+    0x2223: '|',
+    0x2236: ':',
+    0x223C: '~',
+    0x2264: '<',
+    0x2265: '>',
+    0x2266: '<',
+    0x2267: '>',
+    0x2303: '^',
+    0x2329: '<',
+    0x2758: '|',
+    0x2762: '!',
+    0x27E6: '',
+    0x27E8: '<',
+    0x27E9: '>',
+    0x2983: '{',
+    0x2984: '}',
+    0x3002: '.',
+    0x3003: '"',
+    0x3008: '<',
+    0x3009: '>',
+    0x301B: '',
+    0x301C: '~',
+    0x301D: '"',
+    0x301E: '"',
+    0xFEFF: ' '
+}
 
 
 def convert_unicode(text: str):
-    tmp = ''
-    for char in text:
-        for i in range(len(ascii_map)):
-            if char == ascii_map[i][0]:
-                char = ascii_map[i][1]
-                break
-        if ord(char) < 128:
-            tmp = tmp+''.join(char)
-    return tmp
+    return text.translate(ascii_map)
+    # out = ''
+    # for char in text:
+    #     for i in range(len(ascii_map)):
+    #         if char == ascii_map[i][0]:
+    #             char = ascii_map[i][1]
+    #             break
+    #     if ord(char) < 128:
+    #         out = out + char
+    # return tmp
 
 
 if __name__ == '__main__':
@@ -221,6 +147,7 @@ if __name__ == '__main__':
 
     transcribe_p = subparsers.add_parser('transcribe', help='process dataset')
     transcribe_p.add_argument('dataset', type=str, help='dataset directory or filename')
+    transcribe_p.add_argument('-c', '--data-col', type=int)
     transcribe_p.add_argument('-b', '--batch-size', type=int)
     transcribe_p.add_argument('-s', '--strip-html', action='store_true')
     transcribe_p.add_argument('-u', '--convert-unicode', action='store_true')
@@ -260,19 +187,12 @@ if __name__ == '__main__':
         else:
             device = 'auto'
 
-        translator = ctranslate2.Translator('models/'+args.model_path, device=device or 'auto',
-                                            inter_threads=args.cpu_threads or 16, intra_threads=args.gpu_threads or 16, max_queued_batches=4)
-        tokenizer = transformers.AutoTokenizer.from_pretrained('models/'+args.model_path)
-
-        if hasattr(args, 'prompt'):
+        if args.prompt:
             prompt = args.prompt + ": "
         else:
             prompt = "Rewrite with proper spelling, grammar, and punctuation: "
 
         csv.field_size_limit(sys.maxsize)
-
-        if args.fastpunct:
-            fastpunct = FastPunct()
 
         with open(join(outPath, 'anomalies.txt'), 'w', newline='') as anomalies:
             empty = 0
@@ -294,17 +214,18 @@ if __name__ == '__main__':
                                             escapechar='\\', quotechar='\"', quoting=csv.QUOTE_ALL)
                     else:
                         continue
-                    print("Processing: " + datasets[0][i] + '\n')
+                    print(f"Processing: {datasets[0][i]}")
                     writer = csv.writer(outfile, delimiter=',', doublequote=False,
                                         escapechar='\\', quotechar='\'', quoting=csv.QUOTE_ALL)
 
                     data = list(reader)
+                    data_length = len(data)
                     num_cols = len(data[0])
 
-                    data_length = len(data)
-                    for j in tqdm(range(0, data_length, args.batch_size)):
-                        batch_size = min([args.batch_size, data_length - j])
-                        if ext == '.txt':
+                    if ext == '.txt':
+                        print("Converting txt to csv...")
+                        for j in tqdm(range(0, data_length, args.batch_size)):
+                            batch_size = min([args.batch_size, data_length - j])
                             for k in range(batch_size):
                                 row = data[j + k]
                                 if len(row) > num_cols:
@@ -345,42 +266,87 @@ if __name__ == '__main__':
                                             poor += 1
                                             continue
 
-                        if args.strip_html:
-                            y = 5
-                            for x in range(batch_size):
-                                # for y in range(len(data[j + x])):
-                                soup = BeautifulSoup(data[j + x][y], 'html.parser')
-                                for br in soup.find_all('br'):
-                                    br.replace_with(' ')
-                                plain_text = soup.get_text()
-                                data[j + x][y] = plain_text
-
-                        if args.convert_unicode:
+                    if args.strip_html:
+                        print("Stripping HTML...")
+                        # data = list(reader)
+                        infile.seek(0)
+                        for j in tqdm(range(0, data_length, args.batch_size)):
+                            batch_size = min([args.batch_size, data_length - j])
                             for x in range(batch_size):
                                 for y in range(len(data[j + x])):
-                                    data[j + x][y] = convert_unicode(data[j + x][y])
+                                    if not args.data_col or (args.data_col and args.data_col == y):
+                                        soup = BeautifulSoup(data[j + x][y], 'html.parser')
+                                        for br in soup.find_all('br'):
+                                            br.replace_with(' ')
+                                        plain_text = soup.get_text()
+                                        data[j + x][y] = plain_text
+                        del soup
 
-                        # for i in range(args.batch_size):
+                    if args.convert_unicode:
+                        print("Converting unicode punctuation...")
+                        # data = list(reader)
+                        infile.seek(0)
+                        for j in tqdm(range(0, data_length, args.batch_size)):
+                            batch_size = min([args.batch_size, data_length - j])
+                            for x in range(batch_size):
+                                for y in range(len(data[j + x])):
+                                    if not args.data_col or (args.data_col and args.data_col == y):
+                                        data[j + x][y] = convert_unicode(data[j + x][y])
+
+                    if args.fastpunct:
+                        print("Running fastpunct...")
+                        fastpunct = FastPunct()
+                        # data = list(reader)
+                        infile.seek(0)
+                        for j in tqdm(range(0, data_length, args.batch_size)):
+                            batch_size = min([args.batch_size, data_length - j])
+                            sentences = []
+                            max_len = []
+                            for x in range(batch_size):
+                                sentences.append([])
+                                for y in range(len(data[j + x])):
+                                    if not args.data_col or (args.data_col and args.data_col == y):
+                                        sentences[x].append([])
+                                        sentences[x][y] = nltk.tokenize.sent_tokenize(str(data[j + x][y]))
+                                        sentences[x][y] = fastpunct.punct(sentences[x][y], correct=True)
+                                        joined_result = ""
+                                        for sentence in sentences[x][y]:
+                                            if not joined_result:
+                                                joined_result = sentences
+                                            else:
+                                                joined_result = joined_result + ' ' + sentence
+                                        data[j + x][y] = joined_result
+                        del fastpunct
+
+                    # data = list(reader)
+                    infile.seek(0)
+                    print(f"Loading model {args.model_path}...")
+                    translator = ctranslate2.Translator('models/' + args.model_path, device=device or 'auto',
+                                                        inter_threads=args.cpu_threads or 16,
+                                                        intra_threads=args.gpu_threads or 16, max_queued_batches=4)
+                    print(f"Loading tokenizer...")
+                    tokenizer = transformers.AutoTokenizer.from_pretrained('models/' + args.model_path)
+
+                    print("Processing dataset...")
+                    for j in tqdm(range(0, data_length, args.batch_size)):
+                        batch_size = min([args.batch_size, data_length - j])
                         sentences = []
                         max_len = []
 
                         for x in range(batch_size):
                             sentences.append([])
                             for y in range(len(data[j + x])):
-                                sentences[x].append([])
-                                sentences[x][y] = nltk.tokenize.sent_tokenize(str(data[j + x][y]))
-                                # print(f"{x} {y}")
-                                if args.fastpunct and y == 5:
-                                    sentences[x][y] = fastpunct.punct(sentences[x][y], correct=True)
+                                if not args.data_col or (args.data_col and args.data_col == y):
+                                    sentences[x].append([])
+                                    sentences[x][y] = nltk.tokenize.sent_tokenize(str(data[j + x][y]))
 
                         for y in range(len(data[j])):
-                            max_len.append(0)
-                            max_len[y] = max([len(sentences[x][y]) for x in range(args.batch_size)])
-                            # if len(sentences[x][y]) > max_len[y]:
-                            #     max_len[y] = len(sentences[x][y])
+                            if not args.data_col or (args.data_col and args.data_col == y):
+                                max_len.append(0)
+                                max_len[y] = max([len(sentences[x][y]) for x in range(args.batch_size)])
 
                         for y in range(len(data[j])):
-                            if y == 5:
+                            if not args.data_col or (args.data_col and args.data_col == y):
                                 joined_results = []
                                 for z in range(max_len[y]):
                                     ids = []
@@ -388,16 +354,13 @@ if __name__ == '__main__':
                                     for x in range(batch_size):
                                         # print(f"{x} {y} {z}")
                                         joined_results.append("")
-                                        try:
-                                            if len(sentences[x][y]) > z:
-                                                ids.append(x)
-                                                # input_tokens.append([])
-                                                # s = sentences[x][y][z]
-                                                tmp = tokenizer.encode(prompt + sentences[x][y][z])
-                                                tmp2 = tokenizer.convert_ids_to_tokens(tmp)
-                                                input_tokens.append(tmp2)
-                                        except:
-                                            print(sentences)
+                                        if len(sentences[x][y]) > z:
+                                            ids.append(x)
+                                            # input_tokens.append([])
+                                            # s = sentences[x][y][z]
+                                            tmp = tokenizer.encode(prompt + sentences[x][y][z])
+                                            tmp2 = tokenizer.convert_ids_to_tokens(tmp)
+                                            input_tokens.append(tmp2)
 
                                     async def translate():
                                         async_results = []
@@ -414,13 +377,10 @@ if __name__ == '__main__':
                                             for h in r.hypotheses:
                                                 output_tokens = h  # results[0].hypotheses[0]
                                                 output = str(tokenizer.decode(tokenizer.convert_tokens_to_ids(output_tokens)))
-                                            try:
-                                                if not joined_results[id]:
-                                                    joined_results[id] = output
-                                                else:
-                                                    joined_results[id] = joined_results[id] + ' ' + output
-                                            except:
-                                                print('caught exception')
+                                            if not joined_results[id]:
+                                                joined_results[id] = output
+                                            else:
+                                                joined_results[id] = joined_results[id] + ' ' + output
 
                                         # print(joined_results[0])
                                         # writer.writerow(data[j + x])
